b0dcf5d3eb626745903a98747dc47a3d4c103627
jocelyn pyw <jocelynwotcheu2@gmail.com>
2023-09-27 20:58:53 +0200

    cette liste est deha termine

--- src/Components/ProductListHome.js
+++ src/Components/ProductListHome.js
@@ -9,44 +9,31 @@ const ProductListHome = () => {
     { imgesName: "", property: "" },
   ];
   return (
-    <section id="stat">
+    <section id="stat" style={{ marginTop: "6rem", marginBottom: "6rem" }}>
       <div className="container">
         <div className="row">
           <h1
             className="tittle-level-one"
             style={{ marginTop: "1rem", marginBottom: "1rem" }}
           >
-            F.A.Q
+            DE QUOI PARLONS NOUS
           </h1>
           {/* --------------------------------------------------------- */}
 
-          <div className="col-lg-3 col-md-3  stat-item-container">
-            <div className="stat-item-box-icon-box">
-              <img
-                src="../images/product/product1.jpg"
-                alt="nouvelle lumiere"
-              />
-            </div>
-            <div className="stat-item-box-number">
-              +<span className="counter">133</span>
-            </div>
+          <div className="col-lg-3 col-md-3 ">
+            {/* <div className="stat-item-box-icon-box"> */}
+            <img src="../images/product/product1.jpg" alt="nouvelle lumiere" />
+            {/* </div> */}
+
             <div className="stat-item-box-title">
               Hublot LED Blanc 30 Cm IP65, 1620 Lumens, Conso 18W, Blanc Chaud
             </div>
           </div>
 
           {/* --------------------------------------------------------- */}
-          <div className="col-lg-3 col-md-3  stat-item-container">
-            <div className="stat-item-box-icon-box">
-              <img
-                src="../images/product/product2.jpg"
-                alt="nouvelle lumiere"
-              />
-            </div>
-            <div className="stat-item-box-number">
-              {" "}
-              + <span className="counter">300</span>{" "}
-            </div>
+          <div className="col-lg-3 col-md-3 ">
+            <img src="../images/product/product2.jpg" alt="nouvelle lumiere" />
+
             <div className="stat-item-box-title">
               Projecteur Mural Noir LED IP65, 2700 Lumens, Conso 30W (Éq 160W),
               Blanc Chaud
@@ -54,17 +41,9 @@ const ProductListHome = () => {
           </div>
           {/* --------------------------------------------------------- */}
 
-          <div className="col-lg-3 col-md-3  stat-item-container">
-            <div className="stat-item-box-icon-box">
-              <img
-                src="../images/product/product3.jpg"
-                alt="nouvelle lumiere"
-              />
-            </div>
-            <div className="stat-item-box-number">
-              {" "}
-              +<span className="counter">99</span>%
-            </div>
+          <div className="col-lg-3 col-md-3  ">
+            <img src="../images/product/product3.jpg" alt="nouvelle lumiere" />
+
             <div className="stat-item-box-title">
               Projecteur Mural Noir LED IP65, 4500 Lumens, Conso 50W (Éq 200W),
               Blanc Chaud
@@ -72,16 +51,18 @@ const ProductListHome = () => {
           </div>
           {/* --------------------------------------------------------- */}
 
-          <div className="col-lg-3 col-md-3 stat-item-container">
-            <div className="stat-item-box-icon-box">
-              <img
-                src="../images/product/product4.jpg"
-                alt="nouvelle lumiere"
-              />
+          <div className="col-lg-3 col-md-3 ">
+            <img src="../images/product/product4.jpg" alt="nouvelle lumiere" />
+
+            <div className="stat-item-box-title">
+              Projecteur Mural Noir LED IP65, 9000 Lumens, Conso 100W (Éq 550W),
+              Blanc Chaud
             </div>
-            <div className="stat-item-box-number">
-              +<span className="counter">50</span>
-            </div>
+          </div>
+          {/* --------------------------------------------------------- */}
+          <div className="col-lg-3 col-md-3 ">
+            <img src="../images/product/product5.jpg" alt="nouvelle lumiere" />
+
             <div className="stat-item-box-title">
               Projecteur Mural Noir LED IP65, 9000 Lumens, Conso 100W (Éq 550W),
               Blanc Chaud
